8d0bc0c65134c16a5b1ca3269f526694d81a84c5
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-08-22 01:23:33 +0200

    Messing around with code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,24 @@
 # encoding: utf-8
 
-require 'simplecov'
-SimpleCov.start
-
 require 'webmock/rspec'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.order = 'random'
+end
+
+require 'coveralls'
+require 'simplecov'
+
+if ENV.include?('TRAVIS')
+  Coveralls.wear!
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+end
+
+SimpleCov.start do
+  add_group 'Source', 'lib'
+  add_group 'Unit tests', 'spec/cottus'
+  add_group 'Acceptance tests', 'spec/acceptance'
 end
 
 require 'cottus'
